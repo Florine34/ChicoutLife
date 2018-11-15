6563ee0e4afd4b49a536a1b2517d7defcac45918
Justine <vdpoelstien@gmail.com>
2018-11-15 16:21:33 -0500

    update BDD Todolist

--- app/src/main/java/com/example/flo/chicoutlife/WriteToDoListBDD.java
+++ app/src/main/java/com/example/flo/chicoutlife/WriteToDoListBDD.java
@@ -1,24 +1,30 @@
 package com.example.flo.chicoutlife;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class WriteToDoListBDD {
 
     private RUser user;
+    private FirebaseAuth mAuth;
+    private DatabaseReference toDoListDatabase = FirebaseDatabase.getInstance().getReference("ToDoList");
+    private DatabaseReference tacheDatabase = FirebaseDatabase.getInstance().getReference("Taches");
+
+    private Map<String, String> mapAfaire;
+    private Map<String, String> mapFait;
+
+    private String[][] tab; // 0 = true 1 = false
+
+    private int compteur = 0;
 
     public WriteToDoListBDD(RUser u){
         this.user = u;
     }
 
-
-/*
-        String key = rUserDatabase.push().getKey();
-        RUser user = new RUser(pays, domaineEtude, progEtude, sessionAdmi, nbSession, permisEtude, echangeUni, localisationChicout, travailler);
-
-        Map<String, Object> postValues = user.toMap();
-
-        Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put(key, postValues);
-        rUserDatabase.updateChildren(childUpdates);
- */
     /**
      * Fonction qui écrit les tache à faire suivant les renseigments pris par l'utilisateur
      *
@@ -28,18 +34,24 @@ public class WriteToDoListBDD {
      */
     public void insertInDB(){
 
+
         if(user.getBoolEchangeUniversitaire() == true){
             //
+
         }
         else {
             //
         }
 
         if(user.getBoolPermisEtude() == true){
-            // mettre les taches qu'il faut
+            // mettre les taches qu'il faut tache015
+            putMapAfaire("Tache015", "false");
+            compteur++;
         }
         else {
             //
+            putMapFait("Tache015", "true");
+            compteur++;
         }
 
         if(user.getBoolLocalisationChicout() == true){
@@ -68,5 +80,35 @@ public class WriteToDoListBDD {
 
         //A continuer
 
+        if(compteur == 1){
+            writeNewToDoList(mapAfaire, mapFait);
+        }
+
+    }
+
+    // Insère dans la base de données
+    private void writeNewToDoList(Map<String, String> mafaire, Map<String, String> mfait) {
+
+        // Get User's ID
+        mAuth = FirebaseAuth.getInstance();
+
+        String key = mAuth.getUid();
+
+        ToDoList tdl = new ToDoList(mafaire, mfait);
+
+
+        Map<String, Object> userToAdd = tdl.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(key, userToAdd);
+        toDoListDatabase.updateChildren(childUpdates);
+    }
+
+    private void putMapAfaire(String nom, String valeur) {
+        mapAfaire.put(nom, valeur);
+    }
+
+    private void putMapFait(String nom, String valeur) {
+        mapFait.put(nom, valeur);
     }
 }
