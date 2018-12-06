8d81d9ff3c9543ed6dc1e9dd6253fbdebc9684c6
testhyl <testhyl@hotmail.com>
2018-12-06 17:12:34 -0500

    Check annonces

--- app/src/main/java/com/example/flo/chicoutlife/ChoiceAnnonce.java
+++ app/src/main/java/com/example/flo/chicoutlife/ChoiceAnnonce.java
@@ -12,12 +12,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.ArrayMap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -47,10 +49,12 @@ public class ChoiceAnnonce extends Fragment {
 
     FirebaseDatabase database;
     DatabaseReference annonces;
+    DatabaseReference infoPages;
     FirebaseStorage storage ;
     private StorageReference mStorageRef;
     private View racine;
     private static Intent intentAnnonce;
+    ArrayList<String> tableauParametre;
 
     public static Fragment newInstance(){return new ChoiceAnnonce();}
 
@@ -82,6 +86,7 @@ public class ChoiceAnnonce extends Fragment {
         database = FirebaseDatabase.getInstance();
         storage =  FirebaseStorage.getInstance();
 
+        remplirTableauParametres();
         //TODO faire en recevant des parametres cocher selon type si non general
         recyclerView = (RecyclerView) racine.findViewById(R.id.linearMiniaturesAnnonces);
 
@@ -91,6 +96,12 @@ public class ChoiceAnnonce extends Fragment {
 
     public  List<ModelAnnonce> fillWithAnnonce(){
 
+
+
+
+
+
+        //Annonces choisie a afficher
         annonces = database.getReference("Annonces");
         annonces.addValueEventListener(new ValueEventListener() {
             @Override
@@ -101,45 +112,55 @@ public class ChoiceAnnonce extends Fragment {
                     modelsAnnonces = new ArrayList<>();
                     verticalAdapter = new VerticalAdapter(modelsAnnonces,getContext());
 
+                    DataSnapshot dataTags = annonce.child("Tags");
+                    ArrayMap mapTags= new ArrayMap<>();
+                    //Liste des tags de l'annonce en cours
+                    for(DataSnapshot Tag : dataTags.getChildren()){
+                            mapTags.put((String) Tag.getKey() , Tag.getValue());
+                    }
+
+
                     String cheminImg = (String)annonce.child("Image").getValue();
                     mStorageRef =  storage.getReference();
                     StorageReference imageRef = mStorageRef.child(cheminImg);
 
+                    if(annonceValidateByParameter(mapTags,tableauParametre)) {
 
-                    final long ONE_MEGABYTE = 4096 * 4096;
-                    imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                        @Override
-                        public void onSuccess(byte[] bytes) {
+                        final long ONE_MEGABYTE = 4096 * 4096;
+                        imageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                            @Override
+                            public void onSuccess(byte[] bytes) {
 
-                            /*Recuperation des donnes textes dans la base de donnee*/
-                            String modelTitre = (String) annonce.child("Titre").getValue();
-                            String modelDescription = (String) annonce.child("Description").getValue();
-                            if(modelDescription.length()>150){
-                                modelDescription=modelDescription.substring(0,149);
-                                modelDescription = modelDescription + "...";
+                                /*Recuperation des donnes textes dans la base de donnee*/
+                                String modelTitre = (String) annonce.child("Titre").getValue();
+                                String modelDescription = (String) annonce.child("Description").getValue();
+                                if (modelDescription.length() > 150) {
+                                    modelDescription = modelDescription.substring(0, 149);
+                                    modelDescription = modelDescription + "...";
+                                }
+                                ;
+                                /*Recuperation du chemin de l'annonce pour la redirection lors du OnClick*/
+                                String cheminAnnonceBdd = annonce.getKey();
+                                Bitmap imageAnnonce = null;
+
+                                /*Recuperation de l'image dans la base de donnee*/
+                                imageAnnonce = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                imageAnnonce = Bitmap.createScaledBitmap(imageAnnonce, 360, 240, false);
+                                ModelAnnonce model = new ModelAnnonce(modelTitre, modelDescription, imageAnnonce, cheminAnnonceBdd);
+                                modelsAnnonces.add(model);
+
+                                /*Sert pour le recyclerView , sinon la taille est initialiser a zero et le recycler ne se remplit pas*/
+                                verticalAdapter.notifyDataSetChanged();
+
                             }
-                            ;
-                            /*Recuperation du chemin de l'annonce pour la redirection lors du OnClick*/
-                            String cheminAnnonceBdd = annonce.getKey();
-                            Bitmap imageAnnonce =null;
-
-                            /*Recuperation de l'image dans la base de donnee*/
-                            imageAnnonce = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                            imageAnnonce = Bitmap.createScaledBitmap(imageAnnonce,360,240,false);
-                            ModelAnnonce model = new ModelAnnonce(modelTitre, modelDescription,imageAnnonce,cheminAnnonceBdd);
-                            modelsAnnonces.add(model);
-
-                            /*Sert pour le recyclerView , sinon la taille est initialiser a zero et le recycler ne se remplit pas*/
-                            verticalAdapter.notifyDataSetChanged();
-
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            // Handle any errors
-                            Log.e("passage","failure dans choiceAnnonce " + exception.getMessage());
-                        }
-                    });
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle any errors
+                                Log.e("passage", "failure dans choiceAnnonce " + exception.getMessage());
+                            }
+                        });
+                    }
 
 
                 }
@@ -226,5 +247,59 @@ public class ChoiceAnnonce extends Fragment {
         }
     }
 
+    public void remplirTableauParametres(){
+
+        //Parametres de la page de base choisie
+
+        if (intentAnnonce.getStringArrayExtra("TYPE_INTENT").equals("accesbyintent")) {
+            infoPages = database.getReference("InfosPages");
+            infoPages.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    DataSnapshot tagsPage = dataSnapshot.child(intentAnnonce.getStringExtra("NOM_PAGE")).child("Tags");
+                    tableauParametre = new ArrayList<String>();
+                    for (DataSnapshot dataTagsPage : tagsPage.getChildren()) {
+                        tableauParametre.add(dataTagsPage.getKey());
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }else{
+            //Parametres choisie par l'utilisateur
+
+            List<CheckBox> items = new ArrayList<CheckBox>();
+            items.add((CheckBox)racine.findViewById(R.id.checkBoxTagAppartement));
+            items.add((CheckBox)racine.findViewById(R.id.checkBoxTagElectronique));
+            items.add((CheckBox)racine.findViewById(R.id.checkBoxTagNourriture));
+            items.add((CheckBox)racine.findViewById(R.id.checkBoxTagVetements));
+
+            for (CheckBox item : items){
+                if(item.isChecked())
+                    tableauParametre.add((String)item.getContentDescription());
+            }
+
+
+        }
+
+    }
+    public boolean annonceValidateByParameter(ArrayMap arrayMap , ArrayList<String> parametreRequest){
+
+
+       //Parametre de l'objet a afficher
+
+        int nbrParam = parametreRequest.size();
+        int i;
+        for(i= 0 ; i<nbrParam; i++){
+            Log.d("passage" , "Dans annonceValidateByParameter" +arrayMap.get(parametreRequest.get(i)).getClass());
+            if(!(boolean)arrayMap.get(parametreRequest.get(i)) ){
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
--- app/src/main/java/com/example/flo/chicoutlife/FragmentsSwipeAdapter.java
+++ app/src/main/java/com/example/flo/chicoutlife/FragmentsSwipeAdapter.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
 
 import com.google.android.gms.common.server.converter.StringToIntConverter;
 
@@ -20,6 +21,7 @@ public class FragmentsSwipeAdapter extends FragmentPagerAdapter {
         super(fm);
         this.intentInfoAnnonce= intent;
         this.nombrePages = (Integer.parseInt(intentInfoAnnonce.getStringExtra("NOMBRE_PAGE")));
+        Log.d("passage","Dans fragmentsSwipeAdapter " + intentInfoAnnonce.getStringExtra("NOM_PAGE"));
     }
 
     @Override
