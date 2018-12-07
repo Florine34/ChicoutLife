45105a57189f00555c9559c3130fd73a1f7239ba
testhyl <testhyl@hotmail.com>
2018-12-07 02:14:19 -0500

    Ajout du scindement entre todolistActivity et ChoixAnnonces. TODO : Definir juste todolisteActivity en portrait TODO : Quand scinder en 2 click sur la todolist change viewPager de droit TODO : retour en arriere selon portrait ou paysage TODO : Paysage scinder en deux si tablette

--- app/src/main/java/com/example/flo/chicoutlife/ToDoListInfosAnnonces.java
+++ app/src/main/java/com/example/flo/chicoutlife/ToDoListInfosAnnonces.java
@@ -1,14 +1,26 @@
 package com.example.flo.chicoutlife;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class ToDoListInfosAnnonces extends AppCompatActivity {
-
+    private ViewPager pages;
+    private PagerAdapter adapterPages;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.todolist_et_infoannonces);
+        pages = findViewById(R.id.viewPagerOrientation);
+
+        Intent intent = new Intent();
+        intent.putExtra("NOMBRE_PAGE","2");
+        intent.putExtra("NOM_PAGE" ,"Tache002");
+        intent.putExtra("TYPE_INTENT","accesbyintent");
+        adapterPages = new FragmentsSwipeAdapter(getSupportFragmentManager(),intent);
+        pages.setAdapter(adapterPages);
     }
 }
