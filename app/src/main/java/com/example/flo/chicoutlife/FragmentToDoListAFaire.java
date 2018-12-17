eb7a2a9abc315e2b987d9542213644b4ea320feb
testhyl <testhyl@hotmail.com>
2018-12-17 09:41:24 -0500

    -

--- app/src/main/java/com/example/flo/chicoutlife/FragmentToDoListAFaire.java
+++ app/src/main/java/com/example/flo/chicoutlife/FragmentToDoListAFaire.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -109,6 +110,7 @@ public class FragmentToDoListAFaire extends Fragment {
                 tachesaFaire = new ArrayList<>();
                 listAdapteraFaire = new TacheArrayAdapter(getActivity(),context,tachesaFaire);
 
+                Log.d("passage","mAuth :" + mAuth.getUid());
                 DataSnapshot tbToDoListUser = dataSnapshot.child("ToDoList").child((mAuth.getUid()));
                 DataSnapshot aFaire = tbToDoListUser.child("AFaire");
                 DataSnapshot dataTache = dataSnapshot.child("Taches");
